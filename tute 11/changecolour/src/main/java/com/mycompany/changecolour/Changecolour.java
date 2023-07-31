12d96b2600a19c25735ae379f11efa75edaf301a
pawanjana <136308864+pawanjana@users.noreply.github.com>
2023-07-31 18:42:33 +0530

    Add files via upload

---
+++ tute 11/changecolour/src/main/java/com/mycompany/changecolour/Changecolour.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.changecolour;
+
+/**
+ *
+ * @author user
+ */
+public class Changecolour {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
